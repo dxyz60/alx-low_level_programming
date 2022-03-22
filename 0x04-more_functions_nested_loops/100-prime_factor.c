4082c08cf2b56c1e18d3091c3f6ba9531de812c0
dxyz60 <odhiambdavid74@gmail.com>
2022-03-22 22:06:28 +0300

    0x02 late

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,9 @@
+#include "main.h" 
+/** * print_number -
+ prints an integer using _putchar(using recursion)
+  * @n: integer to be printed * */ void print_number(int n) 
+  { unsigned int num = n; /* print '-' in front of -ve integers and ensure n is +ve */ 
+  if (n < 0) { _putchar('-'); num *= -1; } /* 
+  if num has more than 1-digit, print preceding digit first */ 
+  if (num >= 10) print_number(num / 10); _putchar('0' + (num % 10)); }
+  
