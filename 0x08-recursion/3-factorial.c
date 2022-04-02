082fa89661befdfceea0cf60f7208c1a8e3efcbf
David Ochieng Odhiambo <odhiambdavid74@gmail.com>
2022-04-02 11:58:00 +0300

    late consistency

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,23 +1,16 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * factorial - finds factorial
- * @n: int
- * Return: int
+ * factorial - returns the factorial of a given number.
+ * @n: input number.
+ * Return: factorial of the number.
  */
-
 int factorial(int n)
 {
-
-if (n < 0)
-{
-	return (-1);
-}
-else if (n == 0)
-{
-	return (1);
-}
-
-return (n * factorial(n - 1));
-
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (1);
+	else
+		return (n * factorial(n - 1));
 }
